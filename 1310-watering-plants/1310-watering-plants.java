05b39ca63c221babe64d495fc77fb14eb2c47589
SHRITIJ REDDY RAMIDI <shritucode@gmail.com>
2024-09-07 14:07:21 +0530

    Time: 0 ms (100%), Space: 42.6 MB (25.09%) - LeetHub

--- 1310-watering-plants/1310-watering-plants.java
+++ 1310-watering-plants/1310-watering-plants.java
@@ -4,24 +4,16 @@ class Solution {
         int steps=0;
         for(int i =0;i<plants.length;i++){
             if(w>=plants[i]){
-                System.out.println(plants[i]);
                 
             w=w-plants[i];
             steps=steps+1;
-            System.out.println("w" + w);
-            System.out.println("s" + steps);
                 
             }
             else{
-                System.out.println(plants[i] + "  x");
-                w=capacity;
+                w=capacity-plants[i];
                 steps=steps+(2*i)+1;
-                w=w-plants[i];
                 
                 
-            System.out.println("s" + steps);
-                
-            System.out.println("w" + w);
             }
 
         }return steps;
