7928116870b4000873949772dfe1361a39de6927
SHRITIJ REDDY RAMIDI <shritucode@gmail.com>
2024-09-09 00:24:34 +0530

    Time: 542 ms (5.06%) | Memory: 56.1 MB (82.61%) - LeetSync

--- 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.java
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.java
@@ -1,11 +1,11 @@
-import java.util.Arrays;
-
 class Solution {
     public int maxCoins(int[] piles) {
         Arrays.sort(piles);
         int sum = 0;
-        for (int i = piles.length - 2; i > piles.length / 3 - 1; i -= 2) {
-            sum += piles[i];
+        for (int i = piles.length - 2; i >piles.length / 3 - 1; i = i - 2) {
+            sum = sum + piles[i];
+            System.out.println(i);
+            System.out.println("c" + piles[i]);
         }
         return sum;
     }
