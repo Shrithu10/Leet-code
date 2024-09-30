c3efee4fa7f89b8a8527ef744bc2afa0af686a34
SHRITIJ REDDY RAMIDI <shritucode@gmail.com>
2024-09-30 16:42:39 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (6.99%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,21 +1,15 @@
 class Solution {
-    public static int rev(int x){
-        long s=0;
-        while(x>0){
+ 
+    public int reverse(int x) {
+long s=0;
+        while(x!=0){
             s=(s*10)+x%10;
             x=x/10;
 
         }
-    if(s>=Integer.MAX_VALUE){
+           
+    if(s>=Integer.MAX_VALUE || s<Integer.MIN_VALUE){
         return 0;
     }
     return (int)s;
-    }
-    public int reverse(int x) {
-
-        if(x<0){
-            return -rev(-x);
-        }
-        return rev(x);
-        }
-}
+    }}
