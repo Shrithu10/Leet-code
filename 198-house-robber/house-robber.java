59a6bdfa52aa4666726bda469c59113de4cf1f73
SHRITIJ REDDY RAMIDI <shritucode@gmail.com>
2024-10-11 15:35:41 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (97.77%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+        ArrayList<Integer> dp=new ArrayList<>();
+
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        dp.add(nums[0]);
+        dp.add(Math.max(nums[0],nums[1]));
+        for(int i=2;i<nums.length;i++){
+            dp.add(Math.max(dp.get(i-1),dp.get(i-2)+nums[i]));
+        }
+        return dp.get(nums.length-1);
+    }
+}
