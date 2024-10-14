562c9d063555f1f44e337a1baf527c900ca1f66d
SHRITIJ REDDY RAMIDI <shritucode@gmail.com>
2024-10-14 12:12:14 +0530

    Time: 2658 ms (5.06%) | Memory: 57 MB (94.94%) - LeetSync

---
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minStoneSum(int[] nums, int k) {
+        int s=0;
+        PriorityQueue<Integer> max=new PriorityQueue<>((a,b)->b-a);
+        for(int i=0;i<nums.length;i++){
+            max.add(nums[i]);
+            s=s+nums[i];
+        }
+        while(k>0){
+            int n=max.poll();
+            System.out.println(n-n/2);
+            max.add(n-n/2);
+            s=s-n/2;
+            k--;
+        }
+        return s;
+    
+    }
+}
