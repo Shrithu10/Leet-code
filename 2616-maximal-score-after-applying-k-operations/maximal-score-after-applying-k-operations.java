5f2adb70beb008ffd135619a6efe97bb1e77441c
SHRITIJ REDDY RAMIDI <shritucode@gmail.com>
2024-10-14 14:24:27 +0530

    Time: 112 ms (100.00%) | Memory: 63.4 MB (20.38%) - LeetSync

--- 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.java
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.java
@@ -1,6 +1,5 @@
 class Solution {
     public long maxKelements(int[] nums, int k) {
-        System.out.println(nums.length);
         long s=0;
         PriorityQueue<Integer> max=new PriorityQueue<>((a,b)->b-a);
         for(int i=0;i<nums.length;i++){
@@ -9,8 +8,7 @@ class Solution {
         while(k>0){
             int n=max.poll();
             s+=n;
-            max.add((int)Math.ceil((double)n/3));
-
+            max.add(((n + 2)/3));
             k--;
         }
         return s;
