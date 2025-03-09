5ffb11ddadf8a813f46764847182d40f1d69a863
SHRITIJ REDDY RAMIDI <shritucode@gmail.com>
2025-03-09 12:21:50 +0530

    Time: 15 ms (77.17%) | Memory: 58.4 MB (10.55%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -4,11 +4,12 @@ class Solution {
         int l=0;
         int r=skill.length-1;
         long sum=0;
-        HashMap<Integer,Integer> hm=new HashMap<>();
+        //HashMap<Integer,Integer> hm=new HashMap<>();
+        int x=skill[l]+skill[r];
         while(l<=r){
-            int x=skill[l]+skill[r];
-            hm.put(x,hm.getOrDefault(x,0)+1);
-            if(hm.size()>1) return -1;
+            int y=skill[l]+skill[r];
+            // hm.put(x,hm.getOrDefault(x,0)+1);
+            if(x!=y) return -1;
             sum=sum+(skill[l]*skill[r]);
             l++;
             r--;
