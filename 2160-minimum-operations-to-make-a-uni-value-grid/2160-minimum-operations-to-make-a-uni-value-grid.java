e607f8c606c0e5cc8dcbda8d6e64b0299b81e195
SHRITIJ REDDY RAMIDI <shritucode@gmail.com>
2025-03-28 01:12:34 +0530

    Time: 28 ms (92.88%), Space: 77.8 MB (27.82%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
@@ -4,14 +4,12 @@ class Solution {
         int[] nums = new int[m * n];
         int index = 0;
 
-        // Flatten the grid into a 1D array
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 nums[index++] = grid[i][j];
             }
         }
 
-        // Check if all elements have the same remainder when divided by x
         int remainder = nums[0] % x;
         for (int num : nums) {
             if (num % x != remainder) {
@@ -19,18 +17,13 @@ class Solution {
             }
         }
 
-        // Normalize the grid elements by their difference from the remainder
         for (int i = 0; i < nums.length; i++) {
             nums[i] = Math.abs(nums[i] - remainder);
         }
 
-        // Sort the normalized array
         Arrays.sort(nums);
 
-        // Find the median element
         int median = nums[nums.length / 2];
-
-        // Calculate the total number of operations to make all elements equal to the median
         int operations = 0;
         for (int num : nums) {
             operations += Math.abs(num - median) / x;
