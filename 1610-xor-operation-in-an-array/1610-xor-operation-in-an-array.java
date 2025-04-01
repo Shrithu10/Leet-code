f0e768390ac95276d9c93b5130c491ee99329ad0
SHRITIJ REDDY RAMIDI <shritucode@gmail.com>
2025-04-01 18:41:27 +0530

    Time: 0 ms (100%), Space: 40.2 MB (93.08%) - LeetHub

---
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int s=0;
+       for(int i=0;i<n;i++){
+            s=s^(start+2*i);
+       }
+       return s; 
+    }
+}
